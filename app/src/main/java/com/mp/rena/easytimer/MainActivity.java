2f0b20c13a7b05b842a080dacc9ad0030aa388c6
reinal <ff14purinnnn@gmail.com>
2018-12-25 13:15:33 -0800

    Get timer/stop button working and improved how it's displayed

--- app/src/main/java/com/mp/rena/easytimer/MainActivity.java
+++ app/src/main/java/com/mp/rena/easytimer/MainActivity.java
@@ -1,21 +1,21 @@
 package com.mp.rena.easytimer;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    CountDownTimer timer;
     Button starBtb;
-    Button spa;
-    Button softegg;
-    Button hardegg;
-    Button potato;
     EditText timerView;
     boolean timerOn = false;
+    GridLayout grid;
 
     public void startTimer(View view){
         if (!timerOn){
@@ -23,16 +23,25 @@ public class MainActivity extends AppCompatActivity {
             timerOn = true;
             starBtb.setText("STOP!");
             int time = Integer.parseInt(timerView.getText().toString())* 60 * 1000;
-            CountDownTimer timer = new CountDownTimer(time+100, 1000) {
+            timer = new CountDownTimer(time+100, 1000) {
                 @Override
                 public void onTick(long l) {
-                    timerView.setText(String.valueOf((int)l/1000));
+                    int min = (int) l/1000/60;
+                    int seconds = (int)l/1000 - min*60;
+                    String minString = String.valueOf(min);
+                    String secondsString = String.valueOf(seconds);
+                    if (seconds < 10){
+                        secondsString = "0" + secondsString;
+                    }
+                    timerView.setText(minString + ":" + secondsString);
                 }
 
                 @Override
                 public void onFinish() {
                     //start music
-                    timerView.setText("0");
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.sound);
+                    mp.start();
+                    timerView.setText("1");
                     starBtb.setText("START!");
                     timerOn = false;
                 }
@@ -41,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
         } else {
             //stop
+            timerView.setText("1");
             timerOn = false;
+            timer.cancel();
             starBtb.setText("START!");
         }
     }
@@ -54,38 +65,29 @@ public class MainActivity extends AppCompatActivity {
 
         starBtb = findViewById(R.id.startBtn);
         timerView = findViewById(R.id.clockTextView);
-        spa = findViewById(R.id.btn1);
-        softegg = findViewById(R.id.btn2);
-        hardegg = findViewById(R.id.btn3);
-        potato = findViewById(R.id.btn4);
 
-        spa.setOnClickListener(new Button.OnClickListener() {
+        grid = findViewById(R.id.gridLayout);
+
+        grid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                timerView.setText("9");
+                timer.cancel();
+                switch (view.getTag().toString()){
+                    case "spaghetti":
+                        timerView.setText("9");
+                        break;
+                    case "soft":
+                        timerView.setText("6");
+                        break;
+                    case "hard":
+                        timerView.setText("12");
+                        break;
+                    case "potato":
+                        timerView.setText("15");
+                        break;
+
+                }
             }
         });
-        softegg.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                timerView.setText("6");
-            }
-        });
-        hardegg.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                timerView.setText("12");
-            }
-        });
-        potato.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                timerView.setText("15");
-            }
-        });
-
-
-
-
     }
 }
