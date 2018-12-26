276a27c038799202c3db0d2e461b1aeafccc35ab
reinal <ff14purinnnn@gmail.com>
2018-12-25 19:21:08 -0800

    Used service to make timer run in background

---
+++ app/src/main/java/com/mp/rena/easytimer/BroadcastService.java
@@ -0,0 +1,58 @@
+package com.mp.rena.easytimer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.util.Log;
+
+import static com.mp.rena.easytimer.MainActivity.time;
+
+public class BroadcastService extends Service {
+    private final static String TAG = "BroadcastService";
+    public static final String COUNTDOWN_BR = "com.mp.rena.easytimer";
+    Intent bi = new Intent(COUNTDOWN_BR);
+
+    CountDownTimer timer = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.i(TAG, "Starting timer...");
+
+        timer = new CountDownTimer(time+100, 1000) {
+            @Override
+            public void onTick(long l) {
+                Log.i(TAG, "Countdown seconds remaining: " + l / 1000);
+                bi.putExtra("countdown", l);
+                sendBroadcast(bi);
+            }
+
+            @Override
+            public void onFinish() {
+                bi.putExtra("countdown", 0.0);
+                sendBroadcast(bi);
+                Log.i(TAG, "Timer finished");
+            }
+        }.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        timer.cancel();
+        Log.i(TAG, "Timer cancelled");
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+}
--- app/src/main/java/com/mp/rena/easytimer/MainActivity.java
+++ app/src/main/java/com/mp/rena/easytimer/MainActivity.java
@@ -1,58 +1,42 @@
 package com.mp.rena.easytimer;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    CountDownTimer timer;
     Button starBtb;
     EditText timerView;
     boolean timerOn = false;
     GridLayout grid;
+    static int time = 1000;
 
     public void startTimer(View view) {
         if (!timerOn) {
             //start timer
             timerOn = true;
             starBtb.setText("STOP!");
-            int time = Integer.parseInt(timerView.getText().toString()) * 60 * 1000;
-            timer = new CountDownTimer(time + 100, 1000) {
-                @Override
-                public void onTick(long l) {
-                    int min = (int) l / 1000 / 60;
-                    int seconds = (int) l / 1000 - min * 60;
-                    String minString = String.valueOf(min);
-                    String secondsString = String.valueOf(seconds);
-                    if (seconds < 10) {
-                        secondsString = "0" + secondsString;
-                    }
-                    timerView.setText(minString + ":" + secondsString);
-                }
-
-                @Override
-                public void onFinish() {
-                    //start music
-                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.sound);
-                    mp.start();
-                    timerView.setText("1");
-                    starBtb.setText("START!");
-                    timerOn = false;
-                }
-            }.start();
-
+            registerReceiver(br, new IntentFilter(BroadcastService.COUNTDOWN_BR));
+            time = Integer.parseInt(timerView.getText().toString()) * 60 * 1000;
+            startService(new Intent(this, BroadcastService.class));
+            Log.i("i", "Started service");
 
         } else {
             //stop
             timerView.setText("1");
             timerOn = false;
-            timer.cancel();
+            unregisterReceiver(br);
+            stopService(new Intent(this, BroadcastService.class));
             starBtb.setText("START!");
         }
     }
@@ -65,13 +49,79 @@ public class MainActivity extends AppCompatActivity {
 
         starBtb = findViewById(R.id.startBtn);
         timerView = findViewById(R.id.clockTextView);
-
         grid = findViewById(R.id.gridLayout);
 
     }
 
+    private BroadcastReceiver br = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateGUI(intent);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(br, new IntentFilter(BroadcastService.COUNTDOWN_BR));
+        Log.i("i", "Registered broacast receiver");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(br);
+        Log.i("i", "Unregistered broacast receiver");
+    }
+
+    @Override
+    public void onStop() {
+        try {
+            unregisterReceiver(br);
+        } catch (Exception e) {
+            // Receiver was probably already stopped in onPause()
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, BroadcastService.class));
+        Log.i("i", "Stopped service");
+        super.onDestroy();
+    }
+
+    private void updateGUI(Intent intent){
+        Log.i("i", "update gui");
+        if (intent.getExtras() != null) {
+            if(intent.getLongExtra("countdown", 0) == 0L){
+                Log.i("i", "lastcall");
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.sound);
+                mp.start();
+                timerView.setText("1");
+                starBtb.setText("START!");
+                timerOn = false;
+                unregisterReceiver(br);
+                stopService(new Intent(this, BroadcastService.class));
+            } else{
+                long millisUntilFinished = intent.getLongExtra("countdown", 0);
+                Log.i("tag", "Countdown seconds remaining: " +  millisUntilFinished / 1000);
+                int min = (int) millisUntilFinished / 1000 / 60;
+                int seconds = (int) millisUntilFinished / 1000 - min * 60;
+                String minString = String.valueOf(min);
+                String secondsString = String.valueOf(seconds);
+                if (seconds < 10) {
+                    secondsString = "0" + secondsString;
+                }
+                timerView.setText(minString + ":" + secondsString);
+            }
+        }
+    }
+
     public void buttonSelected(View view) {
-        timer.cancel();
+        unregisterReceiver(br);
+        stopService(new Intent(this, BroadcastService.class));
+        timerOn=false;
         switch (view.getTag().toString()) {
             case "spaghetti":
                 timerView.setText("9");
