535de250a59adcbe7dd7de4584d19e8e495e4204
reinal <ff14purinnnn@gmail.com>
2018-12-25 19:21:41 -0800

    added canary leak

---
+++ app/src/main/java/com/mp/rena/easytimer/EasyTimerApplication.java
@@ -0,0 +1,20 @@
+package com.mp.rena.easytimer;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class EasyTimerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+}
